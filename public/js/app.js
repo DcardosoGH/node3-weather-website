4b942e6149928db21c6e429a551aec0a58ced81d
Diogo Cardoso <diogocardoso@Diogos-MacBook-Pro.local>
2019-05-21 11:09:13 +0100

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client JS is loaded!");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const message = document.querySelector("#message");
